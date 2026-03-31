406105efc6d7bf3af130a78e581911c38efbe7c0
masalimov <16955379+masalimov@users.noreply.github.com>
2026-03-31 18:05:59 +0500

    Add setDone function

--- src/todolist.tsx
+++ src/todolist.tsx
@@ -1,42 +1,52 @@
 interface Todo {
-   title: string;
-   desc: string;
-   image: string;
-   done: boolean;
-   createdAt: string;
-   key: number;
+  title: string;
+  desc: string;
+  image: string;
+  done: boolean;
+  createdAt: string;
+  key: number;
 }
 
 interface TodoProps {
-   todos: Todo[];
+  todos: Todo[];
+  setDone: (key: number) => void;
 }
 
-function TodoList({ todos }: TodoProps) {
-   return (
-      <section>
-         <h1>Дела</h1>
-         <table>
-            <tbody>
-               {todos.map((item: Todo) => (
-                  <tr key={item.key}>
-                     <td>
-                        {item.done && <del>{item.title}</del>}
-                        {!item.done && item.title}
-                     </td>
-                     <td>
-                        <button title="Выполнено" disabled={item.done}>
-                           &#9745;
-                        </button>
-                     </td>
-                     <td>
-                        <button title="Удалить">&#9746;</button>
-                     </td>
-                  </tr>
-               ))}
-            </tbody>
-         </table>
-      </section>
-   );
+function TodoList({ todos, setDone }: TodoProps) {
+  return (
+    <section>
+      <h1>Дела</h1>
+      <table className="table is-hoverable is-fullwidth">
+        <tbody>
+          {todos.map((item: Todo) => (
+            <tr key={item.key}>
+              <td>
+                {item.done && <del>{item.title}</del>}
+                {!item.done && item.title}
+              </td>
+              <td>
+                <button
+                  className="button is-success"
+                  title="Выполнено"
+                  disabled={item.done}
+                  onClick={() => {
+                    setDone(item.key);
+                  }}
+                >
+                  &#9745;
+                </button>
+              </td>
+              <td>
+                <button className="button is-danger" title="Удалить">
+                  &#9746;
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
 }
 
 export default TodoList;
