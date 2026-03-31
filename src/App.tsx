557064699ebf958a2bcac9e3b5240ab0e677995c
masalimov <16955379+masalimov@users.noreply.github.com>
2026-03-31 18:09:59 +0500

    Add .editorconfig and reformat files

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,26 @@ import todos from "./todos";
 import TodoList from "./todolist";
 
 function App() {
-  const setDone = (key: number) => {
-    const deed = todos.find((current) => current.key === key);
-    if (deed) {
-      deed.done = true;
-    }
-  };
-  return (
-    <>
-      <div className="container">
-        <nav className="navbar is-light">
-          <div className="navbar-brand">
-            <span className="navbar-item is-uppercase">Todos</span>
-          </div>
-        </nav>
-        <main className="content px-6 py-6">
-          <TodoList todos={todos} setDone={setDone} />
-        </main>
-      </div>
-    </>
-  );
+   const setDone = (key: number) => {
+      const deed = todos.find((current) => current.key === key);
+      if (deed) {
+         deed.done = true;
+      }
+   };
+   return (
+      <>
+         <div className="container">
+            <nav className="navbar is-light">
+               <div className="navbar-brand">
+                  <span className="navbar-item is-uppercase">Todos</span>
+               </div>
+            </nav>
+            <main className="content px-6 py-6">
+               <TodoList todos={todos} setDone={setDone} />
+            </main>
+         </div>
+      </>
+   );
 }
 
 export default App;
--- src/todolist.tsx
+++ src/todolist.tsx
@@ -1,52 +1,52 @@
 interface Todo {
-  title: string;
-  desc: string;
-  image: string;
-  done: boolean;
-  createdAt: string;
-  key: number;
+   title: string;
+   desc: string;
+   image: string;
+   done: boolean;
+   createdAt: string;
+   key: number;
 }
 
 interface TodoProps {
-  todos: Todo[];
-  setDone: (key: number) => void;
+   todos: Todo[];
+   setDone: (key: number) => void;
 }
 
 function TodoList({ todos, setDone }: TodoProps) {
-  return (
-    <section>
-      <h1>Дела</h1>
-      <table className="table is-hoverable is-fullwidth">
-        <tbody>
-          {todos.map((item: Todo) => (
-            <tr key={item.key}>
-              <td>
-                {item.done && <del>{item.title}</del>}
-                {!item.done && item.title}
-              </td>
-              <td>
-                <button
-                  className="button is-success"
-                  title="Выполнено"
-                  disabled={item.done}
-                  onClick={() => {
-                    setDone(item.key);
-                  }}
-                >
-                  &#9745;
-                </button>
-              </td>
-              <td>
-                <button className="button is-danger" title="Удалить">
-                  &#9746;
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
-  );
+   return (
+      <section>
+         <h1>Дела</h1>
+         <table className="table is-hoverable is-fullwidth">
+            <tbody>
+               {todos.map((item: Todo) => (
+                  <tr key={item.key}>
+                     <td>
+                        {item.done && <del>{item.title}</del>}
+                        {!item.done && item.title}
+                     </td>
+                     <td>
+                        <button
+                           className="button is-success"
+                           title="Выполнено"
+                           disabled={item.done}
+                           onClick={() => {
+                              setDone(item.key);
+                           }}
+                        >
+                           &#9745;
+                        </button>
+                     </td>
+                     <td>
+                        <button className="button is-danger" title="Удалить">
+                           &#9746;
+                        </button>
+                     </td>
+                  </tr>
+               ))}
+            </tbody>
+         </table>
+      </section>
+   );
 }
 
 export default TodoList;
