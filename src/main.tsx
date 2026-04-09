197afdde9048812946766869216ed9166ccefb10
masalimov <16955379+masalimov@users.noreply.github.com>
2026-04-09 15:22:53 +0500

    Очистка лишних комментов

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'bulma/css/bulma.css';
-// import App from './App.tsx';
 import { RouterProvider } from 'react-router';
+
+import 'bulma/css/bulma.css';
 import router from './routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      {/* <App /> */}
       <RouterProvider router={router} />
    </StrictMode>,
 );
