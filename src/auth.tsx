4bfdc022c6434ca74c9e2f6e8bf8109001a48a32
masalimov <16955379+masalimov@users.noreply.github.com>
2026-04-16 14:20:22 +0500

    Добавление функций входа/выхода

--- src/auth.tsx
+++ src/auth.tsx
@@ -2,6 +2,8 @@ import {
    getAuth,
    createUserWithEmailAndPassword,
    onAuthStateChanged,
+   signInWithEmailAndPassword,
+   signOut,
    type User,
 } from 'firebase/auth';
 import { FirebaseError } from 'firebase/app';
@@ -33,4 +35,29 @@ export async function register({ request }: ActionFunctionArgs) {
 
 export function setStateChangeHandler(func: (__user: User) => void) {
    return onAuthStateChanged(auth, func);
+}
+
+export async function login({ request }: ActionFunctionArgs) {
+   const fd = await request.formData();
+
+   try {
+      const email = fd.get('email') as string;
+      const pass = fd.get('password') as string;
+      if (email && pass) {
+         await signInWithEmailAndPassword(auth, email, pass);
+      }
+      return redirect('/');
+   } catch (error) {
+      if (error instanceof FirebaseError) {
+         return error.code;
+      } else {
+         // Handle cases where something else was thrown (e.g., a string)
+         console.log('An unexpected error occurred', error);
+      }
+   }
+}
+
+export async function logout() {
+   await signOut(auth);
+   return redirect('/login');
 }
